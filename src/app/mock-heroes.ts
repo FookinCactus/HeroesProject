05ecb24c8331a17449fb08e94a84708ff748d2ae
Ben <ben@s56.net>
2022-02-28 05:56:20 -0700

    FinishedPt3and4

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,7 +4,7 @@ export const HEROES: Hero[] = [
   { id: 11, name: 'Dr Kind' },
   { id: 12, name: 'Florb' },
   { id: 13, name: 'Terrorist' },
-  { id: 14, name: 'Al Pastor Fries' },
+  { id: 14, name: 'Frites' },
   { id: 15, name: 'Clone Drone' },
   { id: 16, name: 'DangerZone' },
   { id: 17, name: 'Flite' },
